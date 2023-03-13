dca0c89508527aef929ffe61984bc71ec459a023
Leônidas <leonidas19992011@hotmail.com>
2023-03-13 08:50:40 -0300

    Details puxando dados da api para utilizacao

--- src/app/pages/details/details.component.ts
+++ src/app/pages/details/details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { PokeApiService } from './../../service/poke-api.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.scss']
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit{
 
+  private urlPokemon: string = 'https://pokeapi.co/api/v2/pokemon'
+  private urlName: string = 'https://pokeapi.co/api/v2/pokemon-species';
+  constructor(
+    private ActivatedRoute: ActivatedRoute,
+    private PokeApiService: PokeApiService){}
+  ngOnInit(): void {
+    this.pokemon;
+  }
+  get pokemon(){
+    const id = this.ActivatedRoute.snapshot.params['id'];
+    const pokemon = this.PokeApiService.apiGetPokemons(`${this.urlPokemon}/${id}`);
+    const name = this.PokeApiService.apiGetPokemons(`${this.urlName}/${id}`);
+
+    return forkJoin([pokemon, name]).subscribe(
+      res => {
+        console.log(res);
+      }
+    )
+  }
 }
